4afd7d544f4771cd1f1bbfbfff21e9e658ef5b33
valdecircarvalho <valdecir.carvalho@outlook.com>
2024-02-16 16:30:06 -0300

    update site code

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ echo "Updating site code @ github repo"
 git pull
 git add .
 git commit -m "update site code"
-git push
+git push --force
 echo "Updating site code @ webserver"
 scp -r .\docs\*  ubuntu@129.148.45.54:/home/ubuntu/producao/site-viaarto-corp/nginx-src
 scp -r .\config\*  ubuntu@129.148.45.54:/home/ubuntu/producao/site-viaarto-corp/nginx-src
